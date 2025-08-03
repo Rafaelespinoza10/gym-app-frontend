7826796470bde238b5de96bbcf3baa5bdd19deda
Rafael_Espinoza~ <rafael.moreno.espinoza10@gmail.com>
2025-08-02 21:33:03 -0600

    fix: add the functions helpers in utils.ts

--- src/dashboard/libs/utils.ts
+++ src/dashboard/libs/utils.ts
@@ -3,3 +3,17 @@ export function cn(...classes: (string | false | null | undefined)[]) {
     return classes.filter(Boolean).join(" ");
   }
   
+ export const handleError = async (response: Response, fallbackMessage: string) => {
+    const error = await response.json().catch(() => ({}));
+    throw new Error(error.message || fallbackMessage);
+  };
+  
+  //authorization
+ export const getAuthHeaders = (): HeadersInit => {
+      const token = localStorage.getItem('token');
+      return {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        };
+  }
+  
