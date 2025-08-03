b98d9d3bb3489898966f1fffee4381330617fa55
Rafael_Espinoza~ <rafael.moreno.espinoza10@gmail.com>
2025-08-02 21:30:34 -0600

    feat: add the exercise reducer

---
+++ src/dashboard/slices/exercisesSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { Exercise } from "../interfaces/exercises";
+import { getAllExercisesThunk } from "../thunks/exercisesThunk";
+
+interface ExerciseState{
+    exercises: Exercise[];
+    loading: boolean;
+    error: string | null;
+}
+
+const initialState: ExerciseState = {
+    exercises: [],
+    loading: false,
+    error: null,
+};
+
+const exercisesSlice = createSlice({
+    name: "exercises",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder 
+        .addCase(getAllExercisesThunk.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(getAllExercisesThunk.fulfilled, (state, action) => {
+            state.loading = false;
+            state.exercises = action.payload;
+        })
+        .addCase(getAllExercisesThunk.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload as string; 
+        })
+    }
+});
+
+export default exercisesSlice.reducer;
