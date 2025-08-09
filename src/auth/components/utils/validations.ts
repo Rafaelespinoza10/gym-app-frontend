ac38bc94422510db4aff788f7c301e539b26265e
Rafael_Espinoza~ <rafael.moreno.espinoza10@gmail.com>
2025-08-09 01:24:41 -0600

    fix: changes the password validations

--- src/auth/components/utils/validations.ts
+++ src/auth/components/utils/validations.ts
@@ -3,9 +3,9 @@
 export const emailValidation =  /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 // at least one Mayus, one minus, one number, one symbol, 8 chars with minimus.
-export const strongPasswordValidation =  /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+export const strongPasswordValidation =   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/;
 
-export const passwordMessage = 'Password must be at least 8 characters and include uppercase, lowercase, number, and special character';
+export const passwordMessage = 'Password must be at least 6 characters and include uppercase, lowercase, number, and special character';
 
 
 export const userNameValidation =  /^[a-zA-Z0-9-_]{3,}$/;
